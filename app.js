10081ba452116a6dc338caaa336acdb3d658b617
kiars1 <deadhaze317@yandex.ru>
2021-11-06 22:04:46 +0700

    CORCS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
@@ -12,11 +13,6 @@ require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 
-const allowedCors = [
-  'http://localhost:3000',
-  'http://mesto.kiars1.nomoredomains.work',
-];
-
 const app = express();
 
 app.use(helmet());
@@ -28,24 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  return next();
-});
+app.use(cors());
 
 app.use(requestLogger);
 
