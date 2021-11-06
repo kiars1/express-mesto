5b5be4b58493d1c6c0909f83a1a12547ac01dd84
kiars1 <deadhaze317@yandex.ru>
2021-11-06 19:59:55 +0700

    CORCS

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
