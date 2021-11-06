15b6c9fb95467ea9ef6f8acc773162b3301a0d6f
kiars1 <deadhaze317@yandex.ru>
2021-11-06 18:46:22 +0700

    CORCS

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const regExp = require('./regexp/regexp');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const allowedCors = [
-  'http://localhost:3000',
-  'http://mesto.kiars1.nomoredomains.work/',
-];
-
+app.use(cors());
 app.use(helmet());
 
 app.use(bodyParser.json());
@@ -25,24 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  return next();
-});
+app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -73,6 +55,8 @@ app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
 
 app.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый Ресурс не найден.')));
+
+app.use(errorLogger);
 
 app.use(errors());
 
