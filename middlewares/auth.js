6265bceb74879ac216f80c36f4cba3f2a5835eb9
kiars1 <deadhaze317@yandex.ru>
2021-11-07 14:04:45 +0700

    FIX

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -10,13 +9,11 @@ module.exports = (req, res, next) => {
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (e) {
     throw new UnauthorizedError('Необходима авторизация');
   }
   req.user = payload;
-
   return next();
 };
